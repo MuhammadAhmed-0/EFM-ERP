fd1dca7db9a7fec6339e4aea07d48bb62f304304
MuhammadAhmed-0 <ma4126403@gmail.com>
2025-07-03 08:55:24 +0500

    --final--

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,11 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "http://192.168.1.60:5173"],
+    origin: [
+      // "http://localhost:5173",
+      // "http://192.168.1.60:5173",
+      "https://erp.efmpvt.com/",
+    ],
     methods: ["GET", "POST"],
   },
 });
@@ -47,8 +51,13 @@ app.use((req, res, next) => {
   req.io = io;
   next();
 });
-app.use(cors());
-app.use(express.json());
+const corsOptions = {
+  origin: "https://erp.efmpvt.com",
+  methods: ["GET", "POST", "PUT", "DELETE"], 
+  allowedHeaders: ["Content-Type", "Authorization"], 
+};
+
+app.use(cors(corsOptions));app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
