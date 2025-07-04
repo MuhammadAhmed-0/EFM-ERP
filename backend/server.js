bc5f12c58b827ae487f51b0f1e864245becc990c
MuhammadAhmed-0 <ma4126403@gmail.com>
2025-07-04 11:02:08 +0500

    --Today's-New-04/07/2025--

--- backend/server.js
+++ backend/server.js
@@ -51,14 +51,14 @@ app.use((req, res, next) => {
   req.io = io;
   next();
 });
-// const corsOptions = {
-//   origin: "https://erp.efmpvt.com",
-//   methods: ["GET", "POST", "PUT", "DELETE"],
-//   allowedHeaders: ["Content-Type", "Authorization"],
-// };
+const corsOptions = {
+  origin: "https://erp.efmpvt.com",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 
-// app.use(cors(corsOptions));
-app.use(cors());
+app.use(cors(corsOptions));
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
